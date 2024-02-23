3008079409b6692f4ce9ed9ed6bf8813c482a3c2
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-02-23 13:51:24 +0000

    messenger feature added

--- src/main_pages/Messenger.jsx
+++ src/main_pages/Messenger.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { StickyNavBar } from '../components/StickyNavBar'
+import { MessengerHeader } from '../components/MessengerHeader'
+
+import ElipseIcon from "../assets/svg/Elipse.svg"
+import TrashIcon from "../assets/svg/Trash.svg"
+import { MessengerSubHeader } from '../components/MessengerSubHeader'
+import { SearchBar } from '../components/SearchBar'
 
 export const Messenger = () => {
   return (
-    <div>Messenger</div>
+    <>
+      <MessengerHeader icon={TrashIcon} secondIcon={ElipseIcon}/>
+      <MessengerSubHeader />
+      <SearchBar />
+    </>
   )
 }

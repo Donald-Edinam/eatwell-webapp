9138dec45c2b05b9fb447074d9e26f47a4eaa463
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-02-28 15:46:35 +0000

    Very happy to know some cool tailwind skills once more

--- src/main_pages/MartEnd.jsx
+++ src/main_pages/MartEnd.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Me_Header } from '../components/martend/Me_Header'
 import { UserCard } from '../components/martend/UserCard'
+import { Me_Affiliate } from '../components/martend/Me_Affiliate'
 
 export const MartEnd = () => {
   return (
     <>
       <Me_Header />
       <UserCard />
+      <Me_Affiliate />
     </>
   )
 }
